00d9563851b5525e0111a37f3920d7b4df4983e5
dhruvupreti05 <dhruvupreti05@gmail.com>
2024-10-24 10:13:28 -0400

    Errors

--- BF log watcher/temp.py
+++ BF log watcher/temp.py
@@ -2,7 +2,7 @@ import os
 
 def get_value(mqtt_subsection, today):
     from Log_watcher import DataSource, log_root
-    
+
     channel = ""
     if mqtt_subsection == "alice_temp_50k": 
         channel = "CH1"
@@ -13,7 +13,7 @@ def get_value(mqtt_subsection, today):
     elif mqtt_subsection == "alice_temp_mxc":
         channel = "CH6"
        
-    path = log_root + today + "\\" + channel + " T " + today + ".log"
+    path = log_root + today + "\\" + channel + "T " + today + ".log"
 
     if not os.path.exists(path) and channel == "CH6": # CH6 may not be present due to Bluefors Config
         return None
